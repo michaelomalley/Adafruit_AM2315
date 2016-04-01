1e45896873edbb76c3b4ca815902e50550007767
Kenneth Lim <kennethlimcp@gmail.com>
2016-04-01 13:57:01 +0800

    enable compile for all platforms
    - moved #include to .h file
    - use PARTICLE flag instead of PHOTON

--- firmware/Adafruit_AM2315.h
+++ firmware/Adafruit_AM2315.h
@@ -1,24 +1,29 @@
-/*************************************************** 
+/***************************************************
   This is a library for the AM2315 Humidity Pressure & Temp Sensor
 
   Designed specifically to work with the AM2315 sensor from Adafruit
   ----> https://www.adafruit.com/products/1293
 
-  These displays use I2C to communicate, 2 pins are required to  
+  These displays use I2C to communicate, 2 pins are required to
   interface
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
   products from Adafruit!
 
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  Written by Limor Fried/Ladyada for Adafruit Industries.
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
 #if (ARDUINO >= 100)
  #include "Arduino.h"
-#else 
+#else
  //#include "WProgram.h"
 #endif
+
+#ifdef PARTICLE
+	#include "application.h"
+#endif
+
 
 #define AM2315_I2CADDR       0x5C
 #define AM2315_READREG       0x03
@@ -30,9 +35,8 @@ class Adafruit_AM2315 {
   float readTemperature(void);
   float readHumidity(void);
   bool readTemperatureAndHumidity(float&, float&);
-  
+
  private:
   bool readData(void);
   float humidity, temp;
 };
-
